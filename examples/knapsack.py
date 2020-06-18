c538cc3e82e87378e1511ca97f5d2118bc95714e
johwiebe <j.wiebe17@imperial.ac.uk>
2020-06-18 10:34:38 +0100

    Restructure example to use reformulation solver.

--- examples/knapsack.py
+++ examples/knapsack.py
@@ -16,7 +16,6 @@ from pyomo.environ import ConcreteModel, Set, Binary, Var, Constraint
 from pyomo.environ import Objective, maximize, SolverFactory, ConstraintList
 from pro import UncSet, UncParam
 from pro.uncset import EllipsoidalSet, PolyhedralSet
-from pro.reformulate import PolyhedralTransformation
 import itertools
 
 
@@ -42,37 +41,13 @@ Adict = {(ti, tj): A[i][j]
          for j, tj in enumerate(tools)}
 perm = itertools.product([1, -1], repeat=len(tools))
 P = [dict(zip(tools, i)) for i in perm]
-rhs = [sum(p[t]*w[t]  for t in tools) + 5.5 for p in P]
+rhs = [sum(p[t]*w[t] for t in tools) + 5.5 for p in P]
 
 
-# # It should be easy to exchange one uncertainty set for another
-# # OPTION 1: w is component of U
-# M.U = UncSet()
-# M.U.w = UncParam(M.ITEMS)
-# w = M.U.w
-# OPTION 2: w is component of M and gets U as argument
 M.U = UncSet()
 M.Ulib = EllipsoidalSet(mu, Adict)
 M.w = UncParam(M.ITEMS, uncset=M.U)
-# M.w.set_uncset(M.U2) # Update uncertainty set like this
 w = M.w
-# # OPTION 3: w is component of M, connection between w and U is implicit
-# M.U = UncSet()
-# M.w = UncParam(M.ITEMS)
-# w = M.w
-# 
-# # OPTION 4: U gets w as argument
-# # Let's got with this one for starters
-# M.w = UncParam(M.ITEMS)
-# M.U = UncSet(param=M.w)
-# M.U2 = UncSet(param=M.w)
-# w = M.w
-# 
-# # # OPTION 5: no UncParam
-# M.U = UncSet(M.ITEMS)
-# w = M.U
-# # or something like
-# w = M.U.get_params()
 
 # Ellipsoidal uncertainty set direct: (w - mu)^T * A * (w - mu) <= 1
 lhs = 0
@@ -89,13 +64,13 @@ for i, p in enumerate(P):
     M.P.cons.add(sum(M.w[t]*p[t] for t in tools) <= rhs[i])
 # library
 M.Plib = PolyhedralSet(P, rhs)
-# M.w._uncset = M.P
 
 M.value = Objective(expr=sum(v[i]*M.x[i] for i in M.ITEMS), sense=maximize)
 M.weight = Constraint(expr=sum(w[i]*M.x[i] for i in M.ITEMS) <= limit)
 
-t = PolyhedralTransformation()
-t.apply_to(M)
-solver = SolverFactory('gurobi')
-solver.solve(M)
-M.x.pprint()
+# Set UncSet
+M.w.uncset = M.P
+
+solver = SolverFactory('pro.robust.reformulation')
+solver.solve(M, options={'solver': 'gurobi'})
+M.pprint()
