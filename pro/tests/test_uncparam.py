170856b4bc344dc97c26ad7b92af9afb056f5972
johwiebe <j.wiebe17@imperial.ac.uk>
2020-07-06 15:39:16 +0100

    Add tests UncParam.

--- pro/tests/test_uncparam.py
+++ pro/tests/test_uncparam.py
@@ -9,6 +9,12 @@ class TestUncParam(unittest.TestCase):
         m.p = pro.UncParam()
         m.pnom = pro.UncParam(nominal=3)
         self.assertEqual(m.pnom.nominal, 3)
+        self.assertEqual(m.p.nominal, None)
+        self.assertFalse(m.p.is_constant())
+        self.assertTrue(m.p.is_potentially_variable())
+        self.assertTrue(m.p.is_variable_type())
+        self.assertTrue(m.p.is_parameter_type())
+        self.assertIs(m.p.ctype, pro.UncParam)
 
     def test_indexed_uncparam(self):
         m = pe.ConcreteModel()
@@ -16,3 +22,10 @@ class TestUncParam(unittest.TestCase):
         m.p.construct()
         self.assertEqual(m.p[0].nominal, 3)
         self.assertEqual(m.p[1].nominal, 4)
+        self.assertIs(m.p[0].ctype, pro.UncParam)
+        self.assertIs(m.p[1].ctype, pro.UncParam)
+        self.assertIs(m.p.ctype, pro.UncParam)
+        self.assertFalse(m.p[0].is_constant())
+        self.assertTrue(m.p[0].is_potentially_variable())
+        self.assertTrue(m.p[0].is_variable_type())
+        self.assertTrue(m.p[0].is_parameter_type())
