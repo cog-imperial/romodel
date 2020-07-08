2516389a22381d214b0eca31f335f314af80570b
johwiebe <j.wiebe17@imperial.ac.uk>
2020-07-08 10:47:28 +0100

    Turn of printing.

--- pro/tests/test_e2e.py
+++ pro/tests/test_e2e.py
@@ -7,29 +7,29 @@ class TestE2E(unittest.TestCase):
     def test_knapsack_reformulation(self):
         m = pro.examples.Knapsack()
         solver = pe.SolverFactory('pro.robust.reformulation')
-        solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
 
     def test_knapsack_cuts(self):
         m = pro.examples.Knapsack()
         solver = pe.SolverFactory('pro.robust.cuts')
-        # solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
 
     def test_portfolio_reformulation(self):
         m = pro.examples.Portfolio()
         solver = pe.SolverFactory('pro.robust.reformulation')
-        solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
 
     def test_portfolio_cuts(self):
         m = pro.examples.Portfolio()
         solver = pe.SolverFactory('pro.robust.cuts')
-        solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
 
     def test_pooling_reformulation(self):
         m = pro.examples.Pooling()
         solver = pe.SolverFactory('pro.robust.reformulation')
-        solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
 
     def test_pooling_cuts(self):
         m = pro.examples.Pooling()
         solver = pe.SolverFactory('pro.robust.cuts')
-        solver.solve(m, tee=True)
+        solver.solve(m, tee=False)
