7b854a228c281d5f1c0a94ba9d30368dc4721a9c
johwiebe <j.wiebe17@imperial.ac.uk>
2020-07-08 14:43:22 +0100

    Fix portfolio example.

--- pro/examples/portfolio.py
+++ pro/examples/portfolio.py
@@ -9,10 +9,6 @@ def Portfolio():
     N = 5
     index = list(range(N))
     mean = [0.1, 0.3, 0.5, 0.7, 0.4]
-    # mean = [0.1, 0.3, 0.5, 0.7, 0.4]
-    # P = np.matrix([])
-    # U = pro.uncset.Polyhedral(I, mean, P)
-    # m.r = pro.UncertainParam(I, uncset=U)
 
     m.x = pe.Var(index, bounds=(0, 1))
     m.z = pe.Var(within=pe.PositiveReals)
@@ -26,11 +22,11 @@ def Portfolio():
     m.U.cons = pe.Constraint(expr=expr <= 0.0005)
 
     m.Obj = pe.Objective(expr=m.z, sense=pe.maximize)
-    x0 = m.x[0]
-    expr = x0*3
+    # x0 = m.x[0]
+    # expr = x0*3
     expr = sum([m.x[i] for i in index]) == 1
     m.cons.add(expr)
-    m.cons.add(-sum([r[i]*m.x[i] for i in index]) <= -m.z)
+    m.cons.add(sum([r[i]*m.x[i] for i in index]) >= m.z)
 
     return m
 
