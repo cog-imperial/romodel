102f527753fa62f177108d0b653c33c0d238a3c5
johwiebe <j.wiebe17@imperial.ac.uk>
2021-03-31 14:29:39 +0100

    split standard and warped gp reformulation

--- romodel/reformulate/__init__.py
+++ romodel/reformulate/__init__.py
@@ -2,4 +2,5 @@ from .base import (BaseRobustTransformation, GeneratorTransformation,
                    NominalTransformation, UnknownTransformation)
 from .ellipsoidal import EllipsoidalTransformation
 from .polyhedral import PolyhedralTransformation
-from .gp import GPTransformation, WGPTransformation
+from .gp import GPTransformation
+from .warpedgp import WGPTransformation
