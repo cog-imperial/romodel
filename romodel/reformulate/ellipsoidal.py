7c169d28f620e0e9a1f30a8d04ecfeb82c1822d8
johwiebe <j.wiebe17@imperial.ac.uk>
2021-03-31 12:10:38 +0100

    added reformulation function ellips for single constraint

--- romodel/reformulate/ellipsoidal.py
+++ romodel/reformulate/ellipsoidal.py
@@ -1,4 +1,4 @@
-from pyomo.environ import Constraint, Var, quicksum, sqrt, Objective
+from pyomo.environ import Constraint, Var, quicksum, sqrt, Objective, Block
 from romodel.reformulate import BaseRobustTransformation
 from pyomo.core import TransformationFactory
 from itertools import chain
@@ -8,7 +8,72 @@ from romodel.util import collect_uncparam
 @TransformationFactory.register('romodel.ellipsoidal',
                                 doc="Ellipsoidal Counterpart")
 class EllipsoidalTransformation(BaseRobustTransformation):
-    def _apply_to(self, instance, root=False):
+    def _reformulate(self, instance, c, param, uncset, counterpart, root=False):
+        # Check constraint/objective
+        if c.ctype is Constraint:
+            assert not c.equality, (
+                    "Currently can't handle equality constraints yet.")
+        repn = self.generate_repn_param(instance, c)
+        assert repn.is_linear(), (
+                "Constraint {} should be linear in "
+                "unc. parameters".format(c.name))
+
+        # Generate robust counterpart
+        det = quicksum(x[0]*x[1].nominal for x in zip(repn.linear_coefs,
+                                                      repn.linear_vars))
+        det += repn.constant
+        param_var_dict = {id(param): var
+                          for param, var
+                          in zip(repn.linear_vars, repn.linear_coefs)}
+        # padding = sqrt( var^T * cov^-1 * var )
+        padding = quicksum(param_var_dict[id(param[ind_i])]
+                           * uncset.cov[i][j]
+                           * param_var_dict[id(param[ind_j])]
+                           for i, ind_i in enumerate(param)
+                           for j, ind_j in enumerate(param))
+        if c.ctype is Constraint:
+            # For upper bound: det + padding <= b
+            if c.has_ub():
+                counterpart.upper = Block()
+                if root:
+                    expr = det + sqrt(padding) <= c.upper
+                    robust = Constraint(expr=expr)
+                else:
+                    counterpart.upper.padding = Var(bounds=(0, float('inf')))
+                    pvar = counterpart.upper.padding
+                    robust = Constraint(expr=det + pvar <= c.upper())
+                    deterministic = Constraint(expr=padding <= pvar**2)
+                    counterpart.upper.det = deterministic
+                counterpart.upper.rob = robust
+            # For lower bound: det - padding >= b
+            if c.has_lb():
+                counterpart.lower = Block()
+                if root:
+                    expr = det - sqrt(padding) >= c.lower
+                    robust = Constraint(expr=expr)
+                else:
+                    counterpart.lower.padding = Var(bounds=(0, float('inf')))
+                    pvar = counterpart.lower.padding
+                    robust = Constraint(expr=c.lower() <= det - pvar)
+                    deterministic = Constraint(expr=padding <= pvar**2)
+                    counterpart.lower.det = deterministic
+                counterpart.lower.rob = robust
+        else:
+            # For minimization: min det + padding
+            # For maximization: max det - padding
+            sense = c.sense
+            if root:
+                expr = det + c.sense*sqrt(padding)
+                robust = Objective(expr=expr, sense=sense)
+            else:
+                counterpart.padding = Var(bounds=(0, float('inf')))
+                pvar = counterpart.padding
+                robust = Objective(expr=det + sense*pvar, sense=sense)
+                deterministic = Constraint(expr=padding <= pvar**2)
+                counterpart.det = deterministic
+            counterpart.rob = robust
+
+    def _apply_to(self, instance, **kwargs):
         for c in chain(self.get_uncertain_components(instance),
                        self.get_uncertain_components(instance,
                                                      component=Objective)):
@@ -26,75 +91,8 @@ class EllipsoidalTransformation(BaseRobustTransformation):
             if not uncset.is_ellipsoidal():
                 continue
 
-            if c.ctype is Constraint:
-                assert not c.equality, (
-                        "Currently can't handle equality constraints yet.")
-            repn = self.generate_repn_param(instance, c)
-            assert repn.is_linear(), (
-                    "Constraint {} should be linear in "
-                    "unc. parameters".format(c.name))
+            counterpart = Block()
+            setattr(instance, c.name + '_counterpart', counterpart)
+            self._reformulate(instance, c, param, uncset, counterpart, **kwargs)
 
-            # Generate robust counterpart
-            det = quicksum(x[0]*x[1].nominal for x in zip(repn.linear_coefs,
-                                                          repn.linear_vars))
-            det += repn.constant
-            param_var_dict = {id(param): var
-                              for param, var
-                              in zip(repn.linear_vars, repn.linear_coefs)}
-            # padding = sqrt( var^T * cov^-1 * var )
-            padding = quicksum(param_var_dict[id(param[ind_i])]
-                               * uncset.cov[i][j]
-                               * param_var_dict[id(param[ind_j])]
-                               for i, ind_i in enumerate(param)
-                               for j, ind_j in enumerate(param))
-            if c.ctype is Constraint:
-                # For upper bound: det + padding <= b
-                if c.has_ub():
-                    name = c.name + '_counterpart_upper'
-                    if root:
-                        expr = det + sqrt(padding) <= c.upper
-                        counterpart = Constraint(expr=expr)
-                    else:
-                        setattr(instance,
-                                c.name + '_padding',
-                                Var(bounds=(0, float('inf'))))
-                        pvar = getattr(instance, c.name + '_padding')
-                        counterpart = Constraint(expr=det + pvar <= c.upper())
-                        deterministic = Constraint(expr=padding <= pvar**2)
-                        setattr(instance, c.name + '_det_upper', deterministic)
-                    setattr(instance, name, counterpart)
-                # For lower bound: det - padding >= b
-                if c.has_lb():
-                    name = c.name + '_counterpart_lower'
-                    if root:
-                        expr = det - sqrt(padding) >= c.lower
-                        counterpart = Constraint(expr=expr)
-                    else:
-                        setattr(instance,
-                                c.name + '_padding',
-                                Var(bounds=(0, float('inf'))))
-                        pvar = getattr(instance, c.name + '_padding')
-                        counterpart = Constraint(expr=c.lower() <= det - pvar)
-                        deterministic = Constraint(expr=padding <= pvar**2)
-                        setattr(instance, c.name + '_det_lower', deterministic)
-                    setattr(instance, name, counterpart)
-            else:
-                # For minimization: min det + padding
-                # For maximization: max det - padding
-                sense = c.sense
-                name = c.name + '_counterpart'
-                if root:
-                    expr = det + c.sense*sqrt(padding)
-                    counterpart = Objective(expr=expr, sense=sense)
-                else:
-                    setattr(instance,
-                            c.name + '_padding',
-                            Var(bounds=(0, float('inf'))))
-                    pvar = getattr(instance, c.name + '_padding')
-                    counterpart = Objective(expr=det + sense*pvar, sense=sense)
-                    deterministic = Constraint(expr=padding <= pvar**2)
-                    setattr(instance,
-                            c.name + '_det',
-                            deterministic)
-                setattr(instance, name, counterpart)
             c.deactivate()
