67a54ac6b47232c7c4e54bec77702f18eae47aac
ddceruti <amedeo.ceruti@tum.de>
2022-11-11 11:33:04 +0100

    fixed pyomo version compatibility and updated requirements.txt and setup.py accordingly

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='romodel',
-    version='0.0.2',
+    version='0.1.0',
     url='https://github.com/johwiebe/romodel.git',
     author='Johannes Wiebe',
     author_email='j.wiebe17@imperial.ac.uk',
     description='Pyomo robust optimization toolbox',
     packages=find_packages(),
-    install_requires=['pyomo', 'numpy'],
+    install_requires=['pyomo==6.0', 'numpy', 'pyutilib'],
 )
